6206485a3b1beb880d3ca532cd6c85ad2c93a3a2
MuratYurtseven <esabet123@hotmail.com>
2024-05-11 21:43:15 +0300

    Start To Project

---
+++ CinemaSeeker/CinemaSeeker/CinemaSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaSeekerApp.swift
+//  CinemaSeeker
+//
+//  Created by Murat on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CinemaSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
